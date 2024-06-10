9532c1b04b3b08e22c7f59738721a59d24cf26bb
Usama Israr <156895405+Usama1190@users.noreply.github.com>
2024-06-10 11:43:05 -0700

    Updated

---
+++ step01_strong_typing/app.ts
@@ -0,0 +1,21 @@
+// strongly typed syntax
+
+let a : string = "Pakistan";
+
+a = "USA";
+
+let b : number = 9;
+
+let c : boolean = true;
+
+// type interface
+
+let e = "USA";
+
+let f = 10.9;
+
+f = 22;
+
+let g = false;
+
+g = true;
---
+++ step02_const_let/app.ts
@@ -0,0 +1,19 @@
+// use const where variable values do not change
+
+const a = 5;
+
+const b : number = 33;
+
+const c = "best";
+
+// I suggest use let instead of var everywhere , because let has blocked scope
+
+if (true) {
+    let z = 4;
+    // use z
+}
+else {
+    let z = "string";
+}
+
+// console.log("let: ", z);     // Error: z is not defined in this scope
