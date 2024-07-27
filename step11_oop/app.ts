46743c35d9dbffc6a7d87b834cb27d64c6afe1b8
Usama Israr <156895405+Usama1190@users.noreply.github.com>
2024-07-27 12:29:33 -0700

    oop learning on July 27, 24

---
+++ step11_oop/app.ts
@@ -0,0 +1,49 @@
+/* Object Oriented Programming with TypeScript
+
+Tutorial in TypeScript official Documentation link in readme.md file.
+The first link example here.
+
+Classes
+
+Syntax: first write the keyword class and then class name with first
+letter capital. Then open the curly brackets and define the properties.
+Properties are the variables that are used to store the data of the 
+class. */
+
+// class Usama {};
+
+
+
+
+// ______________________________________________________________________
+
+// Class Members or properties
+
+// class Point {
+//     x: number;
+//     y: number;
+// }
+
+// const pt = new Point();
+
+// pt.x = 12;
+// pt.y = 34;
+
+
+// Another example
+
+class Point {
+    x = 0;
+    y = 1;
+}
+
+const pt = new Point();
+
+console.log(pt);
+
+
+
+
+
+
+// ______________________________________________________________________
